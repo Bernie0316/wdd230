d484b6a08936cbbba7a52b89bd9f8cf6b8de1b8b
Bernie Zhong <chu23008@byui.edu>
2024-04-30 21:59:06 +0800

    assignment  ver 1.2

--- scripts/base.js
+++ scripts/base.js
@@ -1,7 +1,15 @@
-// getting location
+// Get the last modified date/time of the document
+var lastModifiedDate = document.lastModified;
+// Display the last modified date/time in the second paragraph
+document.getElementById("lastModified").innerHTML = "Last modified: " + lastModifiedDate;
+
+//////////////////////////////////
+
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(reverseGeocode);
+        navigator.geolocation.getCurrentPosition(function(position) {
+            reverseGeocode(position);
+        });
     } else {
         document.getElementById("location").innerHTML = "Geolocation is not supported by this browser.";
     }
@@ -10,12 +18,12 @@ function getLocation() {
 function reverseGeocode(position) {
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
-    var apiURL = "https://nominatim.openstreetmap.org/reverse?format=json&lat=" + latitude + "&lon=" + longitude;
+    var apiURL = "https://nominatim.openstreetmap.org/reverse?format=json&lat=" + latitude + "&lon=" + longitude + "&accept-language=en"; 
     
     fetch(apiURL)
         .then(response => response.json())
         .then(data => {
-            var city = data.address.city;
+            var city = data.address.city || data.address.town || data.address.village || data.address.hamlet || data.address.locality; 
             var country = data.address.country;
             var locationInfo = city + ", " + country;
             document.getElementById("location").innerHTML = locationInfo;
