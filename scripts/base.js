26751d0a6839738f54a6f4af6e3ab469b137b2d6
Bernie Zhong <chu23008@byui.edu>
2024-05-06 19:05:34 +0800

    menu, fonts update

--- scripts/base.js
+++ scripts/base.js
@@ -34,3 +34,13 @@ function reverseGeocode(position) {
 }
 
 getLocation();
+
+//////////////////////////////////
+// hambergur menu
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
